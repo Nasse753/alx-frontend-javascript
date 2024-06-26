532c088b7dea712895da02acf6e2642c5093924a
Nasse753 <Nasse753@gmail.com>
2024-06-26 13:36:45 -0700

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,21 @@
+export function uploadPhoto() {
+  return Promise.resolve({ body: 'photo-profile-1' });
+}
+
+export function createUser() {
+  return Promise.resolve({ firstName: 'Guillaume', lastName: 'Salva' });
+}
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
